4aac230b38791dd09d4e498d7c9b0781e3adeede
LONGZR007 <LONGZR007@users.noreply.github.com>
2026-04-17 14:55:01 +0000

    feat: 修复地址缩写相关的编译错误

--- src/eproto_packet_node.c
+++ src/eproto_packet_node.c
@@ -1,7 +1,7 @@
 #include "eproto_packet_node.h"
 
 eproto_node_t* eproto_packet_node_create(eproto_malloc_func_t malloc_func, eproto_free_func_t free_func,
-                                         uint8_t source_address, uint8_t destination_address, uint16_t packet_id,
+                                         uint8_t source_addr, uint8_t destination_addr, uint16_t packet_id,
                                          uint8_t* data, uint16_t data_length, eproto_packet_callback_t callback,
                                          void* private_data, uint8_t no_wait, uint8_t packet_type,
                                          uint8_t max_retry_count, uint32_t timeout_ms) {
@@ -10,8 +10,8 @@ eproto_node_t* eproto_packet_node_create(eproto_malloc_func_t malloc_func, eprot
         return NULL;
 
     EPROTO_INIT_LIST_HEAD(&node->list);
-    node->source_address = source_address;
-    node->destination_address = destination_address;
+    node->source_addr = source_addr;
+    node->destination_addr = destination_addr;
     node->packet_id = packet_id;
     node->callback = callback;
     node->private_data = private_data;
